4341e79594952e132e710d3b21b4f0ee34d8af41
Kovvuru Dhanush <kovurudhanush@gmail.com>
2023-04-30 18:55:32 +0530

    first

---
+++ Module2/array.js
@@ -0,0 +1,14 @@
+// array provides you ordered collection of data
+
+var arr = [12, "Dhanush", true, 12.4]
+
+console.log(arr)
+
+var a = arr[1] // accesing the values based on indexing
+console.log(a)
+console.log(arr[3])
+
+arr[1] = "maari"// to change the values of the array 
+console.log(arr[1])
+
+console.log(arr.length)// to print the array length 
